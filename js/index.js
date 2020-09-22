2e00dc1b4cfd57d3d94335f48d4dd6e8b69029db
jonah-fro <jonah.frossard@my-digital-school.org>
2020-09-22 23:30:44 +0200

    feat(display time): display time when we type time in input

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
 const form = document.querySelector('form')
 const input = document.querySelector('input')
 
+const rgxVerify = {
+  hour: /\b(hour)\b/i,
+  date: /\b(date)\b/i,
+  time: /\b(time)\b/i,
+  hello: /\b(hello)\b/i,
+  hey: /\b(hey)\b/i
+}
+
 form.addEventListener('submit', (e) => {
-  e.preventDefault()
+  e.preventDefault()  
+
+  const today = new Date();
+  const time = String(today.getHours()).padStart(2, "0") + ":" + String(today.getMinutes()).padStart(2, "0") + ":" + String(today.getSeconds()).padStart(2, "0");
+  const date = today.getFullYear()+'-'+String(today.getMonth()+1).padStart(2, "0")+'-'+String(today.getDate()).padStart(2, "0");  
 
   if(input.value != ""){
     document.querySelector('.messageArea').insertAdjacentHTML('afterbegin', 
@@ -14,7 +26,22 @@ form.addEventListener('submit', (e) => {
     '</div>')
   } else{
     alert('Please write something.')
-  }
+  }  
+
+  if (rgxVerify.hour.test(input.value) || rgxVerify.time.test(input.value)){  
+   document.querySelector('.messageArea').insertAdjacentHTML('afterbegin', 
+    '<div class="messageBlock">'+
+     '<div class="user">'+
+      '<img src="assets/img/chaton.jpeg" class="messageAvatar">'+
+      '<span class="messageUsername">Cat Time Bot</span>'+
+     '</div>'+
+     '<div class="message">'+
+      '<p class="messageContent">Il est '+time+
+      '</p>'+
+     '</div>'+
+    '</div>');
+  }  
+
   if (rgxVerify.date.test(input.value)){
     document.querySelector('.messageArea').insertAdjacentHTML('afterbegin', 
     '<div class="messageBlock">'+
