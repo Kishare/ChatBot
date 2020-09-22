f029d781ba134dbb8345366fea5606a263bc93fd
Junior <kuakuvidesire@gmail.com>
2020-09-22 23:18:17 +0200

    feat(displayDate): Display date when we type "date" in input

--- js/index.js
+++ js/index.js
@@ -3,13 +3,30 @@ const input = document.querySelector('input')
 
 form.addEventListener('submit', (e) => {
   e.preventDefault()
-  document.querySelector('.messageArea').insertAdjacentHTML('afterbegin', 
-  '<div class="messageResponseBlock">'+
-   '<div class="messageResponse">'+
-    '<p class="messageContent">'+input.value+
-    '</p>'+
-    '<span class="messageDate">15/09/2020</span>'+
-   '</div>'+
-  '</div>');
+
+  if(input.value != ""){
+    document.querySelector('.messageArea').insertAdjacentHTML('afterbegin', 
+    '<div class="messageResponseBlock">'+
+     '<div class="messageResponse">'+
+      '<p class="messageContent">'+input.value+
+      '</p>'+
+     '</div>'+
+    '</div>')
+  } else{
+    alert('Please write something.')
+  }
+  if (rgxVerify.date.test(input.value)){
+    document.querySelector('.messageArea').insertAdjacentHTML('afterbegin', 
+    '<div class="messageBlock">'+
+     '<div class="user">'+
+      '<img src="assets/img/chat2.jpeg" class="messageAvatar">'+
+      '<span class="messageUsername">Cat Date Bot</span>'+
+     '</div>'+
+     '<div class="message">'+
+      '<p class="messageContent">Il est '+date+
+      '</p>'+
+     '</div>'+
+    '</div>');
+  }
   input.value = ""
 })
